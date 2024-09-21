bf15c4ca66b06ccecb1654e45cc5f197db5f860d
mohamuhsin <muhsinohoho@outlook.com>
2024-09-21 17:26:28 +0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Form />
+      <Footer />
     </div>
   );
 }
